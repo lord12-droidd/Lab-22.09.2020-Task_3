e96d72099852527e87774410153403e01c2d8194
Melnyk <melnikdmitriy2002@gmail.com>
2020-09-21 21:04:47 +0300

    Add project files.

---
+++ Task_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Task_3
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag == true)
+            {
+                try
+                {
+                    Console.OutputEncoding = System.Text.Encoding.Unicode;
+                    flag = false;
+                    Console.WriteLine("Введіть змінну типу int ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введіть змінну типу double");
+                    double b = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введіть змінну типу long");
+                    long c = Convert.ToInt64(Console.ReadLine());
+                    Console.WriteLine($"a = {a}; b = {b}; c = {c}");
+                    Console.ReadKey();
+                }
+                catch
+                {
+                    flag = true;
+                    Console.WriteLine("Enter correct value");
+                }
+            }
+
+        }
+    }
+}
